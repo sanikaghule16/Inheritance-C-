8c100c1bc9b7d09b5e4b98a08bf7db6a5e2a51d5
sanikaghule16 <sanikaghule4@gmail.com>
2026-04-09 09:52:56 +0530

    Add Multilevel_Program.cs with main execution logic

---
+++ Multilevel_Program.cs
@@ -0,0 +1,12 @@
+using Inheritance;
+namespace Multilevel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Result r1 = new Result(101, "Prachit", 50, 70);
+            r1.display();
+        }
+    }
+}
